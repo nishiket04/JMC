ed1927d90d9628cb37da6cd4d7dfa0068f54a448
nishiket04 <nishiket04@gmail.com>
2023-10-24 13:32:01 +0530

    added firebase support and checked its working

--- app/src/main/java/com/nishiket/jmc/MainActivity.java
+++ app/src/main/java/com/nishiket/jmc/MainActivity.java
@@ -3,19 +3,40 @@ package com.nishiket.jmc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView txt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txt=findViewById(R.id.txt);
+        // Read from the databasse
         FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
+        DatabaseReference myRef = database.getReference("new");
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                txt.setText(value);
+            }
 
-        myRef.setValue("Hello, World!");
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("fail", "Failed to read value.", error.toException());
+            }
+        });
     }
 }
