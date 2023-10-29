bd35cc472a39545d785ae4f4bd26625b0f2dd1d6
nishiket04 <nishiket04@gmail.com>
2023-10-29 08:58:39 +0530

    made main layout design

--- app/src/main/java/com/nishiket/jmc/user/MainActivity.java
+++ app/src/main/java/com/nishiket/jmc/user/MainActivity.java
@@ -1,26 +1,39 @@
 package com.nishiket.jmc.user;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static com.nishiket.jmc.R.*;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.annotation.SuppressLint;
+import android.app.ActionBar;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.widget.TextView;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 import com.nishiket.jmc.R;
 
 public class MainActivity extends AppCompatActivity {
     TextView txt;
+    ActionBar actionBar;
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        txt=findViewById(R.id.txt);
+        setContentView(layout.activity_main);
+        toolbar=findViewById(id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("JMC");
+        toolbar.setTitleTextColor(getColor(color.white));
+        txt=findViewById(id.txt);
         // Read from the databasse
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("new");
