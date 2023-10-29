69a7aaf494a092c0f8b8c6c49eb5d30635b4686e
nishiket04 <nishiket04@gmail.com>
2023-10-29 18:24:14 +0530

    made some change

--- app/src/main/java/com/nishiket/jmc/views/DelayedFragment.java
+++ app/src/main/java/com/nishiket/jmc/views/DelayedFragment.java
@@ -61,6 +61,6 @@ public class DelayedFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_closed, container, false);
+        return inflater.inflate(R.layout.fragment_delayed, container, false);
     }
 }
